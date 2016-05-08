01fceb0e7cfb28fe0192fd8b798ef9eaccd9c8f5
Michael Alicea <mfalicea@buffalo.edu>
2016-05-08 12:22:47 -0400

    Add a second filter that abreviates the readable time

--- angular-readable-time.js
+++ angular-readable-time.js
@@ -33,6 +33,39 @@
     };
   });
 
+  angular.module('readableTime', []).filter('readableTimeAbv', function() {
+    return function(seconds) {
+      var day, format, hour, minute, month, week, year;
+      seconds = parseInt(seconds, 10);
+      minute = 60;
+      hour = minute * 60;
+      day = hour * 24;
+      week = day * 7;
+      year = day * 365;
+      month = year / 12;
+      format = function(number, string) {
+        string = number === 1 ? string : "" + string + "s";
+        return "" + number + " " + string;
+      };
+      switch (false) {
+        case !(seconds < minute):
+          return format(seconds, '');
+        case !(seconds < hour):
+          return format(Math.floor(seconds / minute), 'min');
+        case !(seconds < day):
+          return format(Math.floor(seconds / hour), 'hr');
+        case !(seconds < week):
+          return format(Math.floor(seconds / day), 'day');
+        case !(seconds < month):
+          return format(Math.floor(seconds / week), 'wk');
+        case !(seconds < year):
+          return format(Math.floor(seconds / month), 'mo');
+        default:
+          return format(Math.floor(seconds / year), 'yr');
+      }
+    };
+  });
+
 }).call(this);
 
 /*
